95be23392133c401c6149af40acbf2deb54b0f26
ThuyLam07 <tlam070909@gmail.com>
2024-11-22 00:36:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,79 +1,67 @@
-let signalingServer = new WebSocket('ws://localhost:8080'); // Địa chỉ WebSocket của server signaling
+    let peerConnection = new RTCPeerConnection()
+    let localStream;
+    let remoteStream;
 
-let peerConnection = new RTCPeerConnection();
-let localStream;
-let remoteStream;
+    let init = async () => {
+        localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:true})
+        remoteStream = new MediaStream()
+        document.getElementById('user-1').srcObject = localStream
+        document.getElementById('user-2').srcObject = remoteStream
 
-let init = async () => {
-    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-    remoteStream = new MediaStream();
-    document.getElementById('user-1').srcObject = localStream;
-    document.getElementById('user-2').srcObject = remoteStream;
-
-    localStream.getTracks().forEach((track) => {
-        peerConnection.addTrack(track, localStream);
-    });
-
-    peerConnection.ontrack = (event) => {
-        event.streams[0].getTracks().forEach((track) => {
-            remoteStream.addTrack(track);
+        localStream.getTracks().forEach((track) => {
+            peerConnection.addTrack(track, localStream);
         });
-    };
 
-    // Lắng nghe tin nhắn đến từ signaling server
-    signalingServer.onmessage = async (message) => {
-        const data = JSON.parse(message.data);
-        if (data.offer) {
-            await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer));
-            const answer = await peerConnection.createAnswer();
-            await peerConnection.setLocalDescription(answer);
-            signalingServer.send(JSON.stringify({ answer: peerConnection.localDescription }));
-        } else if (data.answer) {
-            await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
-        } else if (data.iceCandidate) {
-            try {
-                await peerConnection.addIceCandidate(new RTCIceCandidate(data.iceCandidate));
-            } catch (e) {
-                console.error('Error adding received ICE candidate', e);
+        peerConnection.ontrack = (event) => {
+            event.streams[0].getTracks().forEach((track) => {
+            remoteStream.addTrack(track);
+            });
+        };
+    }
+
+    let createOffer = async () => {
+
+        peerConnection.onicecandidate = async (event) => {
+        
+            if(event.candidate){
+                document.getElementById('offer-sdp').value = JSON.stringify(peerConnection.localDescription)
             }
+        };
+
+        const offer = await peerConnection.createOffer();
+        await peerConnection.setLocalDescription(offer);
+    }
+
+    let createAnswer = async () => {
+
+        let offer = JSON.parse(document.getElementById('offer-sdp').value)
+
+        peerConnection.onicecandidate = async (event) => {
+            //Event that fires off when a new answer ICE candidate is created
+            if(event.candidate){
+                console.log('Adding answer candidate...:', event.candidate)
+                document.getElementById('answer-sdp').value = JSON.stringify(peerConnection.localDescription)
+            }
+        };
+
+        await peerConnection.setRemoteDescription(offer);
+
+        let answer = await peerConnection.createAnswer();
+        await peerConnection.setLocalDescription(answer); 
+    }
+
+    let addAnswer = async () => {
+        console.log('Add answer triggerd')
+        let answer = JSON.parse(document.getElementById('answer-sdp').value)
+        console.log('answer:', answer)
+        if (!peerConnection.currentRemoteDescription){
+            peerConnection.setRemoteDescription(answer);
         }
-    };
-};
+    }
 
-let createOffer = async () => {
-    peerConnection.onicecandidate = (event) => {
-        if (event.candidate) {
-            signalingServer.send(JSON.stringify({ iceCandidate: event.candidate }));
-        }
-    };
+    init()
 
-    const offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
-    signalingServer.send(JSON.stringify({ offer: peerConnection.localDescription }));
-};
+    document.getElementById('create-offer').addEventListener('click', createOffer)
+    document.getElementById('create-answer').addEventListener('click', createAnswer)
+    document.getElementById('add-answer').addEventListener('click', addAnswer)
 
-let createAnswer = async () => {
-    let offer = JSON.parse(document.getElementById('offer-sdp').value);
-
-    peerConnection.onicecandidate = (event) => {
-        if (event.candidate) {
-            signalingServer.send(JSON.stringify({ iceCandidate: event.candidate }));
-        }
-    };
-
-    await peerConnection.setRemoteDescription(offer);
-    const answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(answer);
-    signalingServer.send(JSON.stringify({ answer: peerConnection.localDescription }));
-};
-
-let addAnswer = async () => {
-    const answer = JSON.parse(document.getElementById('answer-sdp').value);
-    await peerConnection.setRemoteDescription(answer);
-};
-
-init();
-
-document.getElementById('create-offer').addEventListener('click', createOffer);
-document.getElementById('create-answer').addEventListener('click', createAnswer);
-document.getElementById('add-answer').addEventListener('click', addAnswer);
